c9bdcdc41f82915196f2240bea18ae9a55e1d593
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-22 21:58:16 +0300

    GetLast3BlogsWithAuthorsList

--- Presentation/CarBook.WebApi/Controllers/BlogsController.cs
+++ Presentation/CarBook.WebApi/Controllers/BlogsController.cs
@@ -45,5 +45,12 @@ namespace CarBook.WebApi.Controllers
             await _mediator.Send(command);
             return Ok("Blog başarıyla güncellendi");
         }
+
+        [HttpGet("GetLast3BlogsWithAuthorsList")]
+        public async Task<IActionResult> GetLast3BlogsWithAuthorsList()
+        {
+            var values = await _mediator.Send(new GetLast3BlogsWithAuthorsQuery());
+            return Ok(values);
+        }
     }
 }
