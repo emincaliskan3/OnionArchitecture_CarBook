e0bfbfd5443670dc5db08e9812c6283d04de2942
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-26 00:10:58 +0300

    admin_footeraddress-location-service-testimonial

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminLocationController.cs
@@ -0,0 +1,91 @@
+﻿using CarBook.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class AdminLocationController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminLocationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7120/api/Locations");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateLocation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLocation(CreateLocationDto createLocationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createLocationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7120/api/Locations", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminLocation", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> RemoveLocation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7120/api/Locations?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminLocation", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> UpdateLocation(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resposenMessage = await client.GetAsync($"https://localhost:7120/api/Locations/{id}");
+            if (resposenMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resposenMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateLocationDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> UpdateLocation(UpdateLocationDto updateLocationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateLocationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7120/api/Locations/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminLocation", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminStatisticsController.cs
@@ -0,0 +1,30 @@
+﻿using CarBook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class AdminStatisticsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminStatisticsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7120/api/Testimonials");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
