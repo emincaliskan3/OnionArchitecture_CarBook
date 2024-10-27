a10f94bade8d8960c2f818465775f851c3ca2df1
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-28 02:23:15 +0300

    reservation_mediator_api

--- Frontends/CarBook.WebUI/Controllers/ReservationController.cs
+++ Frontends/CarBook.WebUI/Controllers/ReservationController.cs
@@ -1,13 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.LocationDtos;
+using CarBook.Dto.ReservationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
     public class ReservationController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ReservationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index(int id)
         {
             ViewBag.v1 = "Araç Kiralama";
             ViewBag.v2 = "Araç Rezervasyon Formu";
+            ViewBag.v3 = id;
+
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7120/api/Locations");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.Name,
+                                                Value = x.LocationID.ToString()
+                                            }).ToList();
+            ViewBag.v = values2;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateReservationDto createReservationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createReservationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7120/api/Reservations", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
