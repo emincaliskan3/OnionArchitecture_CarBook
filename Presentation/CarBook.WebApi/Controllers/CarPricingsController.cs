ac7768f44c2f71a9cdf853fa4eb7e020cde65dee
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-28 04:26:18 +0300

    GetCarPricingWithTimePeriod

--- Presentation/CarBook.WebApi/Controllers/CarPricingsController.cs
+++ Presentation/CarBook.WebApi/Controllers/CarPricingsController.cs
@@ -21,6 +21,13 @@ namespace CarBook.WebApi.Controllers
             return Ok(values);
         }
 
+        [HttpGet("GetCarPricingWithTimePeriodList")]
+        public async Task<IActionResult> GetCarPricingWithTimePeriodList()
+        {
+            var values = await _mediator.Send(new GetCarPricingWithTimePeriodQuery());
+            return Ok(values);
+        }
+
 
     }
 }
