8d990e7821bfe757c17fd4cb89a2f52736e8b7ff
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-30 04:13:22 +0300

    Yeni bir araç eklendiğinde önceden eklenen özellikler gelmiyordu düzeltildi.

--- Core/CarBook.Application/Features/CQRS/Handlers/CarHandlers/CreateCarCommandHandler.cs
+++ Core/CarBook.Application/Features/CQRS/Handlers/CarHandlers/CreateCarCommandHandler.cs
@@ -1,24 +1,28 @@
 ﻿using CarBook.Application.Features.CQRS.Commands.CarCommands;
 using CarBook.Application.Interfaces;
 using CarBook.Domain.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CarBook.Application.Features.CQRS.Handlers.CarHandlers
 {
     public class CreateCarCommandHandler
     {
-        private readonly IRepository<Car> _repository;
-        public CreateCarCommandHandler(IRepository<Car> repository)
+        private readonly IRepository<Car> _carRepository;
+        private readonly IRepository<Feature> _featureRepository;
+        private readonly IRepository<CarFeature> _carFeatureRepository;
+
+        public CreateCarCommandHandler(IRepository<Car> carRepository, IRepository<Feature> featureRepository, IRepository<CarFeature> carFeatureRepository)
         {
-            _repository = repository;
+            _carRepository = carRepository;
+            _featureRepository = featureRepository;
+            _carFeatureRepository = carFeatureRepository;
         }
+
         public async Task Handle(CreateCarCommand command)
         {
-            await _repository.CreateAsync(new Car
+            // Yeni bir araba oluştur ve kaydet
+            var car = new Car
             {
                 BigImageUrl = command.BigImageUrl,
                 Luggage = command.Luggage,
@@ -29,7 +33,23 @@ namespace CarBook.Application.Features.CQRS.Handlers.CarHandlers
                 CoverImageUrl = command.CoverImageUrl,
                 BrandID = command.BrandID,
                 Fuel = command.Fuel
-            });
+            };
+
+            await _carRepository.CreateAsync(car);
+
+            // Tüm mevcut özellikleri getir ve yeni araba için CarFeature ekle
+            var features = await _featureRepository.GetAllAsync();
+            foreach (var feature in features)
+            {
+                var carFeature = new CarFeature
+                {
+                    CarID = car.CarID,
+                    FeatureID = feature.FeatureID,
+                    Available = false // Başlangıç değeri olarak false atıyoruz
+                };
+
+                await _carFeatureRepository.CreateAsync(carFeature);
+            }
         }
     }
 }
